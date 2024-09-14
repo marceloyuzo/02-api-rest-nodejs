9aff9d29e02f05f6ad9aae27bc1d67fa0c41fcb6
marceloyuzo <marceloyuzo0@hotmail.com>
2024-09-13 23:20:21 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Requisitos Funcionais
+
+[x] O usuário deve ser capaz de criar uma nova transação;
+[x] O usuário deve ser capaz de obter um resumo da sua conta;
+[x] O usuário deve ser capaz de listar todas as transações que já ocorreram;
+[x] O usuário deve ser capaz de visualizar uma transação única;
+
+# Regra de Negocio
+
+[x] A transação pode ser do tipo crédito que somará ao valor total, ou débito subtrairá;
+[ ] Deve ser possível identificarmos o usuário entre as requisitações;
+[ ] O usuário só pode visualizar transações o qual ele criou;
---
+++ test/example.spec.ts
@@ -0,0 +1,128 @@
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    expect(cookies).toBeDefined()
+    if (!cookies) {
+      throw new Error('Didnt generated cookies.')
+    }
+
+    const listTransactionsReponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsReponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ])
+  })
+
+  it('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    expect(cookies).toBeDefined()
+    if (!cookies) {
+      throw new Error('Didnt generated cookies.')
+    }
+
+    const listTransactionsReponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsReponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit Transaction',
+        amount: 8000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    expect(cookies).toBeDefined()
+    if (!cookies) {
+      throw new Error('Didnt generated cookies.')
+    }
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit Transaction',
+        amount: 1000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({ amount: 7000 })
+  })
+})
